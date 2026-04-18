7f52228f82c70ada3213b1b3304f0293d8883052
NEELSAMELL369 <neelsamel369@gmail.com>
2026-04-18 17:07:28 +0530

    remove unwanted code files folders

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-// server.ts
-import env from "./config/env";
-import app from "./app";
-const PORT = Number(env.PORT ?? 3000);
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-//# sourceMappingURL=server.js.map
