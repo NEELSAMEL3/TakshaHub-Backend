f885d23fa96f8213db1730bc88fa510fcf3de8be
NEELSAMELL369 <neelsamel369@gmail.com>
2026-04-26 13:43:33 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import type { Application, Request, Response } from "express";
 import routes from "./routes.js";
 import { errorHandler } from "./common/middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
